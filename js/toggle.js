d67311f04585e16ef4b83fb711e1904abd078053
QuentinVB <vanbutsele@intechinfo.fr>
2017-04-26 12:29:20 +0200

    moved menu and add button to toggle menu

---
+++ js/toggle.js
@@ -0,0 +1,33 @@
+function toggle(trigger,container)
+{
+    switch(triggerState)
+    {
+        case true:
+            triggerState = false;
+            container.style.display="none";
+            trigger.style.backgroundImage="url('img/ic_expand_more_black_18dp.png')";
+        break;
+        case false:
+            triggerState = true;
+            container.style.display="block";
+            trigger.style.backgroundImage="url('img/ic_expand_less_black_18dp.png')";
+        break;
+    }
+}
+
+var menu = document.getElementsByClassName("toggleableMenu");
+
+for (var index = 0; index < menu.length; index++) {
+    var element = menu[index];
+    var trigger = element.getElementsByTagName("span")[0];
+    var container = element.getElementsByTagName("div")[0];
+    var triggerState = false;
+    trigger.addEventListener("click", function (e) {             
+            toggle(trigger,container);
+            //xSend = document.getElementById("xPosition").value;
+            //setPosition(xSend,ySend);
+        }
+    );
+    toggle(trigger,container);
+}
+
--- views/viewmap.php
+++ views/viewmap.php
@@ -5,32 +5,35 @@ include("controlers/controler.php");
 include("functions/toolbox.php");
 ?>
     <body>
-		<nav id="data">
-			<ul>
-				<li><a href="backoffice/backoffice.php">Ajouter/Editer Marqueur</a></li>
-				<li>Dernière mise à jour : <?php echo $date[0]; ?></li>
-			</ul>	
-			<h3>Legende</h3>
-			<ul>
-				<li><img src ="img/square-18.png"/>Lieu d'intéret</li>
-				<li><img src ="img/monument-18.png"/>Ruines explorable</li>
-				<li><img src ="img/park2-18.png"/> Biome ou lieu naturel </li>
-				<li><img src ="img/star-18.png"/> Emplacement potentiel de construction</li>
-				<li><img src ="img/circle-stroked-18.png"/>Tracé de fleuve ou lac</li>
-				<li><img src ="img/limite.png"/>Limite de map</li>
-				<li><img src ="img/route.png"/>Ouvrage civil (pont,tunnel...)</li>				
-			</ul>
-			<h3>Où suis-je ?</h3>
-			<p>
-				Entrez vos coordonnés pour vous localiser sur la carte.<br/>
-				<label>X</label> : <input type="number" name="xPosition" id="xPosition" placeholder="0" value="0"/><br/>
-				<label>Z</label> : <input type="number" name="yPosition" id="yPosition" placeholder="0" value="0"/>	
-			</p>
-			<p>
-				<img src ="img/boussole.png"/><br/>
-				http://tiny.cc/20rcky
-			</p>
-			<p>Aquilon, cartographie par Hamil Sambre <br/>aka KaentinWede, dessin par Asaric - 2017</p>
+		<nav id="data" class="toggleableMenu">
+			<span>&nbsp;	</span>
+			<div>
+				<ul>
+					<li><a href="backoffice/backoffice.php">Ajouter/Editer Marqueur</a></li>
+					<li>Dernière mise à jour : <?php echo $date[0]; ?></li>
+				</ul>	
+				<h3>Legende</h3>
+				<ul>
+					<li><img src ="img/square-18.png"/>Lieu d'intéret</li>
+					<li><img src ="img/monument-18.png"/>Ruines explorable</li>
+					<li><img src ="img/park2-18.png"/> Biome ou lieu naturel </li>
+					<li><img src ="img/star-18.png"/> Emplacement potentiel de construction</li>
+					<li><img src ="img/circle-stroked-18.png"/>Tracé de fleuve ou lac</li>
+					<li><img src ="img/limite.png"/>Limite de map</li>
+					<li><img src ="img/route.png"/>Ouvrage civil (pont,tunnel...)</li>				
+				</ul>
+				<h3>Où suis-je ?</h3>
+				<p>
+					Entrez vos coordonnés pour vous localiser sur la carte.<br/>
+					<label>X</label> : <input type="number" name="xPosition" id="xPosition" placeholder="0" value="0"/><br/>
+					<label>Z</label> : <input type="number" name="yPosition" id="yPosition" placeholder="0" value="0"/>	
+				</p>
+				<p>
+					<img src ="img/boussole.png"/><br/>
+					http://tiny.cc/20rcky
+				</p>
+				<p>Aquilon, cartographie par Hamil Sambre <br/>aka KaentinWede, dessin par Asaric - 2017</p>
+			</div>
 		</nav>
 		<div id="mapid"></div>
         <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
@@ -58,7 +61,7 @@ include("functions/toolbox.php");
 				"Limites": limiteX,
 				"Nature": biomeX,
 				"Emplacements":emplacementX,
-				"Fleuves":fleuveX
+				"Fleuves":fleuveX,
 				"Route":routeX
 			};
 			L.control.layers(null,overlays).addTo(map);			
@@ -74,6 +77,7 @@ include("functions/toolbox.php");
 					
 		</script>
 		<script src="js/location2.js"></script>
+		<script src="js/toggle.js"></script>
     </body>
 	
 <?php
